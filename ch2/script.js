4980ede5a1e19268e7bd2913e8a570e51c0aee64
Ashish Kumar Yadav <parthy.here@gmail.com>
2026-05-03 02:14:56 +0530

    js dom project instagram post like animation

--- ch2/script.js
+++ ch2/script.js
@@ -1,90 +1,23 @@
-var arr = [
-   {
-    team  :'CSK',
-    primary :'yellow',
-    secondary :'blue',
-    fullName : 'Chennai Super King',
-    trophies : 5,
-    captain : 'Gaikwad',
-   },
-   {
-    team  :'RCB',
-    primary :'red',
-    secondary :'black',
-    fullName : 'Royal Challengers Bangalore',
-    trophies : 1,
-    captain : 'Patidar',
-   },
-   {
-    team  :'MI',
-    primary :'blue',
-    secondary :'gold',
-    fullName : 'Mumbai Indians',
-    trophies : 5,
-    captain : 'Hardik',
-   },
-   {
-    team  :'KKR',
-    primary :'purple',
-    secondary :'gold',
-    fullName : 'Kolkata Knight Riders',
-    trophies : 3,
-    captain : 'Rahane',
-   },
-   {
-    team  :'RR',
-    primary :'pink',
-    secondary :'blue',
-    fullName : 'Rajasthan Royals',
-    trophies : 1,
-    captain : 'Parag',
-   },
-   {
-    team  :'SRH',
-    primary :'orange',
-    secondary :'black',
-    fullName : 'Sunrisers Hyderabad',
-    trophies : 2,
-    captain : 'kishan',
-   },
-   {
-    team  :'PBKS',
-    primary :'red',
-    secondary :'white',
-    fullName : 'Punjab Kings',
-    trophies : 0,
-    captain : 'shreyas',
-   },
-   {
-    team  :'GT',
-    primary :'navy blue',
-    secondary :'golden',
-    fullName : 'Gujrat Titans',
-    trophies : 1,
-    captain : 'Gill',
-   },
-]
+let img = document.querySelector('img')
+
+var love = document.querySelector('#love')
 
 
 
+img.addEventListener("dblclick",function(){
+  love.style.opacity = 1;
+  love.style.transform = 'translate(-50%, -50%) scale(1) rotate(0)';
 
+setTimeout(function () {
+        love.style.transform = 'translate(-50%,-300%) scale(1) rotate(60deg)'
+    }, 800)
+    setTimeout(function () {
+        love.style.opacity = 0
+    }, 1000)
+    setTimeout(function () {
+        love.style.transform = 'translate(-50%,-50%) scale(0) rotate(-60deg)'
+    }, 1200)
 
-let btn = document.querySelector('button');
-let h1 = document.querySelector('h1');
-let main = document.querySelector('main');
-let fullform = document.querySelector('#fullname')
-let trophies = document.querySelector('#trophies')
-let captain = document.querySelector('#captain')
+ 
 
-
-btn.addEventListener("click",function(){
-    let winner = arr[Math.floor(Math.random()*arr.length)];
-     console.log(winner.team)
-     h1.innerHTML = winner.team
-     h1.style.backgroundColor = winner.primary
-     fullform.innerHTML = winner.fullName
-     trophies.innerHTML = winner.trophies
-     captain.innerHTML = winner.captain
-    
 })
-   
